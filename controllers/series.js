09264f14495b6a0ab83fb46ef09ff951979f449b
Alisson Souza <alissonvsouza8@gmail.com>
2020-01-09 21:20:11 -0200

    Get all series

--- controllers/series.js
+++ controllers/series.js
@@ -1,5 +1,18 @@
-const index = (req, res) => res.render('series/index')
-const newSerie = (req, res) => res.render('series/new')
+const Serie = require('../models/series')
+
+const index = (req, res) => {
+    Serie.find({}, (err, docs) => {
+        res.render('series/index', { series: docs })
+    })
+}
+const newSerie = (req, res) => {
+    const serie = new Serie({
+        name: 'Van Helsing',
+        status: 'watching'
+    })
+    serie.save(() => console.log('saved'))
+    res.render('series/new')
+}
 
 module.exports = {
     index, newSerie
