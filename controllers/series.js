49518f0dad482d7d17a7e7d0c0db5591e888bcaa
Alisson Souza <alissonvsouza8@gmail.com>
2020-01-11 11:42:02 -0200

    Add async/await

--- controllers/series.js
+++ controllers/series.js
@@ -4,41 +4,32 @@ const labels = [
     { id: 'watched', name: 'Assistido' }
 ]
 
-const index = ({ Serie }, req, res) => {
-    Serie.find({}, (err, docs) => {
-        res.render('series/index', { series: docs, labels })
-    })
+const index = async ({ Serie }, req, res) => {
+    const series = await Serie.find({})
+    res.render('series/index', { series, labels })
 }
-const newSerie = ({ Serie }, req, res) => {
+const newSerie = async ({ Serie }, req, res) => {
     const serie = new Serie(req.body)
-    serie.save(() => {
-        res.redirect('/series')
-    })
+    await serie.save()
+    res.redirect('/series')
 }
 const newForm = (req, res) => {
     res.render('series/new')
 }
-const deleteOne = ({ Serie }, req, res) => {
-    Serie.remove({
-        _id: req.params.id
-    }, (err) => {
-        res.redirect('/series')
-    })
+const deleteOne = async ({ Serie }, req, res) => {
+    await Serie.remove({ _id: req.params.id })
+    res.redirect('/series')
 }
-const editSerie = ({ Serie }, req, res) => {
-    Serie.findOne({ _id: req.params.id }, (err, serie) => {
-        serie.name = req.body.name
-        serie.status = req.body.status
-        serie.save()
-        res.redirect('/series')
-    })
+const editSerie = async ({ Serie }, req, res) => {
+    const serie = await Serie.findOne({ _id: req.params.id })
+    serie.name = req.body.name
+    serie.status = req.body.status
+    await serie.save()
+    res.redirect('/series')
 }
-const editForm = ({ Serie }, req, res) => {
-    Serie.findOne({
-        _id: req.params.id
-    }, (err, serie) => {
-        res.render('series/edit', { serie, labels })
-    })
+const editForm = async ({ Serie }, req, res) => {
+    const serie = await Serie.findOne({ _id: req.params.id })
+    res.render('series/edit', { serie, labels })
 }
 
 module.exports = {
